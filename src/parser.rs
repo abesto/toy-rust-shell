2c5479123629a2796b02fd81220f0cb5b07c5c82
Zoltan Nagy <abesto0@gmail.com>
2019-01-13 15:50:23 +0000

    Progress on parser, rudimentary lexing is in place

--- src/parser.rs
+++ src/parser.rs
@@ -1,42 +1,40 @@
 // TODO add the rest of the characters that can start operators
-const OPERATOR_START: &str = "<>$";
 
-mod token_recognition {
+mod split_words {
     /*
      * http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_03
      * Shell Command Language - 2.3: Token Recognition
      * POSIX.1-2017
      */
 
-    use super::OPERATOR_START;
     use std::vec;
 
+    const OPERATOR_START: &str = "<>$&;";
     const BLANK: &str = " \t";
 
     #[derive(Debug)]
     struct State<'a> {
         input: &'a String,
-        tokens: Vec<&'a str>,
-        current_token_start: usize,
-        current: Option<(usize, char)>,
-        iterator: &'a mut std::str::CharIndices<'a>,
+        words: Vec<&'a str>,
+        current_word_start: usize,
+        iterator: &'a mut std::iter::Peekable<std::str::CharIndices<'a>>,
     }
 
     impl<'a> State<'a> {
-        fn current_token(&mut self) -> &'a str {
-            return &self.input[self.current_token_start..self.current_position()];
+        fn current_word(&mut self) -> &'a str {
+            return &self.input[self.current_word_start..self.current_position()];
         }
 
         fn current_position(&mut self) -> usize {
-            return self.current.map_or(self.input.len(), |x| x.0);
+            return self.iterator.peek().map_or(self.input.len(), |x| x.0);
         }
 
         fn current_character(&mut self) -> Option<char> {
-            return self.current.map(|x| x.1);
+            return self.iterator.peek().map(|x| x.1);
         }
 
         fn is_eof(&mut self) -> bool {
-            return self.current.is_none();
+            return self.iterator.peek().is_none();
         }
 
         fn current_character_in(&mut self, class: &str) -> bool {
@@ -49,44 +47,55 @@ mod token_recognition {
         }
 
         fn delimit(&mut self) {
-            if self.current_position() > self.current_token_start {
-                let token = self.current_token();
-                self.tokens.push(token);
-                self.current_token_start = self.current_position();
+            if self.current_position() > self.current_word_start {
+                let token = self.current_word();
+                self.words.push(token);
+                self.current_word_start = self.current_position();
             }
         }
 
         fn skip(&mut self) {
             self.consume();
-            self.current_token_start = self.current_position();
+            self.current_word_start = self.current_position();
         }
 
         fn consume(&mut self) {
-            self.current = self.iterator.next();
+            self.iterator.next();
         }
     }
 
-    pub fn tokenize<'a>(input: &'a String) -> Vec<String> {
+    pub fn scan<'a>(input: &'a String) -> Vec<String> {
         let mut state = State {
             input,
-            tokens: vec![],
-            current_token_start: 0,
-            iterator: &mut input.char_indices(),
-            current: None,
+            words: vec![],
+            current_word_start: 0,
+            iterator: &mut input.char_indices().peekable(),
         };
 
-        state.current = state.iterator.next();
         while !state.is_eof() {
             handle_char(&mut state);
         }
         // Handle EOF
         handle_char(&mut state);
 
-        return state.tokens.iter().map(|s| String::from(*s)).collect();
+        return state.words.iter().map(|s| String::from(*s)).collect();
+    }
+
+    fn extend_operator<'a>(state: &mut State<'a>) -> bool {
+        match state.current_character() {
+            None => false,
+            Some(c) => match (state.current_word(), c) {
+                // TODO add the rest
+                ("&", '&') => true,
+                ("|", '|') => true,
+                (">", '>') => true,
+                ("<", '<') => true,
+                _ => false,
+            },
+        }
     }
 
     fn handle_char<'a>(state: &mut State<'a>) {
-        print!("{:?} ", state.current);
         // 1. If the end of input is recognized, the current token (if any) shall be delimited.
         if state.is_eof() {
             println!("EOF");
@@ -96,8 +105,10 @@ mod token_recognition {
         // current character is not quoted and can be used with the previous
         // characters to form an operator, it shall be used as part of that
         // (operator) token.
-        // TODO
-
+        else if extend_operator(state) {
+            println!("EXTEND OPERATOR");
+            state.consume();
+        }
         // 3. If the previous character was used as part of an operator and the
         // current character cannot be used with the previous characters to form
         // an operator, the operator containing the previous character shall be
@@ -157,7 +168,7 @@ mod token_recognition {
         // character shall be appended to that word.
         // TODO better check for "part of a word"
         else if state
-            .current_token()
+            .current_word()
             .chars()
             .last()
             .map_or(false, |c| !OPERATOR_START.contains(c))
@@ -186,7 +197,7 @@ mod token_recognition {
     #[cfg(test)]
     mod tests {
         fn assert_tokenizes(input: &str, expected: Vec<&str>) {
-            assert_eq!(super::tokenize(&String::from(input)), expected);
+            assert_eq!(super::scan(&String::from(input)), expected);
         }
 
         #[test]
@@ -219,19 +230,18 @@ mod token_recognition {
     }
 }
 
-mod grammar {
+mod lexer {
     /*
+     * Implements parts of
      * https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_10
      * Shell Command Language - 2.10: Shell Grammar
      * POSIX.1-2017
      */
 
-    // Should probably rewrite this using nom
-
-    use super::OPERATOR_START;
+    use std::slice;
 
     #[derive(Debug, PartialEq, Eq)]
-    pub enum Symbol {
+    pub enum Token {
         Operator(char),
         AndIf,
         OrIf,
@@ -239,89 +249,129 @@ mod grammar {
         Word(String),
     }
 
-    type Rule = Fn(&String) -> Option<Symbol>;
-
-    struct Rules<'a> {
-        rules: Vec<&'a Rule>,
+    struct Lexer<'a> {
+        input: &'a Vec<String>,
+        iterator: &'a mut std::iter::Peekable<slice::Iter<'a, String>>,
+        tokens: Vec<Token>,
     }
 
-    impl<'a> Rules<'a> {
-        fn evaluate(&mut self, token: &String) -> Option<Symbol> {
-            for rule in &self.rules {
-                let result = rule(token);
-                if result.is_some() {
-                    return result;
+    impl<'a> Lexer<'a> {
+        // Apply logic, including individual token recognizers, to a lexer, and extract the resulting tokens
+        fn apply(input: &'a Vec<String>, logic: fn(&mut Lexer) -> ()) -> Vec<Token> {
+            let mut l = Lexer {
+                input,
+                iterator: &mut input.iter().peekable(),
+                tokens: vec![],
+            };
+            logic(&mut l);
+            return l.tokens;
+        }
+
+        fn run(input: &'a Vec<String>) -> Vec<Token> {
+            Lexer::apply(input, |lexer| {
+                while lexer.iterator.peek().is_some() {
+                    let _ = lexer.andif()
+                        || lexer.orif()
+                        || lexer.single_char_operator()
+                        || lexer.word();
+                }
+            })
+        }
+
+        // Helpers for individual token recognizers
+        fn recognize(&mut self, token: Token) -> bool {
+            self.tokens.push(token);
+            self.iterator.next();
+            true
+        }
+
+        fn tag(&mut self, expected: &str, token: Token) -> bool {
+            if self.iterator.peek().map_or(false, |next| next == &expected) {
+                self.recognize(token)
+            } else {
+                false
+            }
+        }
+
+        // Individual token recognizers
+        fn andif(&mut self) -> bool {
+            self.tag("&&", Token::AndIf)
+        }
+
+        fn orif(&mut self) -> bool {
+            self.tag("||", Token::OrIf)
+        }
+
+        fn single_char_operator(&mut self) -> bool {
+            match self.iterator.peek() {
+                None => false,
+                Some(word) if word.len() != 1 => false,
+                Some(word) => {
+                    let character = word.chars().next().unwrap();
+                    if "()&;<>".contains(character) {
+                        self.recognize(Token::Operator(character))
+                    } else {
+                        false
+                    }
                 }
             }
-            return None;
         }
-    }
 
-    pub fn parse(tokens: &Vec<String>) -> Vec<Symbol> {
-        let mut rules = Rules {
-            rules: vec![
-                //// 2.10.1 Shell Grammar Lexical Conventions
-                // 1. If the token is an operator, the token identifier for that operator shall result.
-                &single_character_operator,
-                &multi_character_operator,
-                // 2. If the string consists solely of digits and the delimiter
-                // character is one of '<' or '>', the token identifier
-                // IO_NUMBER shall be returned.
-                // TODO
-                //// 2.10.2 Shell Grammar Rules
-                // 1. [Command Name]
-            ],
-        };
-
-        return tokens
-            .iter()
-            .map(|t| {
-                rules
-                    .evaluate(t)
-                    .unwrap_or_else(|| panic!("syntax error near unexpected token `{}'", t))
-            })
-            .collect();
-    }
-
-    fn single_character_operator(token: &String) -> Option<Symbol> {
-        if token.len() != 1 {
-            return None;
-        }
-        let character = token.chars().next().unwrap();
-        if OPERATOR_START.contains(character) {
-            return Some(Symbol::Operator(character));
-        }
-        return None;
-    }
-
-    fn multi_character_operator(token: &String) -> Option<Symbol> {
-        match token.as_str() {
-            "&&" => Some(Symbol::AndIf),
-            "||" => Some(Symbol::OrIf),
-            _ => None,
+        fn word(&mut self) -> bool {
+            self.iterator
+                .next()
+                .map(|word| self.tokens.push(Token::Word(word.clone())));
+            return true;
         }
     }
 
     #[cfg(test)]
     mod tests {
-        use super::super::token_recognition;
-        use super::Symbol;
+        use super::super::split_words;
+        use super::{Lexer, Token};
 
-        fn assert_parses(input: &str, expected: Vec<Symbol>) {
-            let tokens = token_recognition::tokenize(&String::from(input));
-            assert_eq!(super::parse(&tokens), expected);
+        fn assert_apply(input: &str, expected: Vec<Token>, logic: fn(&mut Lexer) -> ()) {
+            let words = &split_words::scan(&input.to_string());
+            assert_eq!(Lexer::apply(words, logic), expected);
+        }
+
+        fn assert_lex(input: &str, expected: Vec<Token>) {
+            let words = &split_words::scan(&input.to_string());
+            assert_eq!(Lexer::run(words), expected);
+        }
+
+        fn word(input: &str) -> Token {
+            Token::Word(input.to_string())
         }
 
         #[test]
-        fn parse_multichar_operators() {
-            assert_parses("&&", vec![Symbol::AndIf]);
-            // TODO test the rest
+        fn lex_word() {
+            assert_apply("foobar", vec![word("foobar")], |l| {
+                l.word();
+            });
         }
 
         #[test]
-        fn parse_singlechar_operators() {
-            assert_parses("<", vec![Symbol::Operator('<')]);
-            // TODO test the rest
+        fn true_and_false() {
+            assert_lex(
+                "true && false",
+                vec![word("true"), Token::AndIf, word("false")],
+            );
+        }
+
+        #[test]
+        fn single_char_op() {
+            assert_lex(
+                "ls; sleep 30\t& ls",
+                vec![
+                    word("ls"),
+                    Token::Operator(';'),
+                    word("sleep"),
+                    word("30"),
+                    Token::Operator('&'),
+                    word("ls"),
+                ],
+            );
         }
     }
 }
